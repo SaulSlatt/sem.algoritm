bc3c8921cf897751ed33638ec3e131d3b8b9a5f1
SaulSlatt <81392882+SaulSlatt@users.noreply.github.com>
2021-03-25 17:05:56 -0600

    Add files via upload

---
+++ Circulos/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DIANA
+ * Date: 16/03/2021
+ * Time: 05:20 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Circulos
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBoximage = new System.Windows.Forms.PictureBox();
+			this.Boton1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.buttonAnalizar = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoximage)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBoximage
+			// 
+			this.pictureBoximage.AccessibleName = "imagen";
+			this.pictureBoximage.Location = new System.Drawing.Point(12, 12);
+			this.pictureBoximage.Name = "pictureBoximage";
+			this.pictureBoximage.Size = new System.Drawing.Size(554, 321);
+			this.pictureBoximage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBoximage.TabIndex = 0;
+			this.pictureBoximage.TabStop = false;
+			// 
+			// Boton1
+			// 
+			this.Boton1.AccessibleName = "ShowImage";
+			this.Boton1.Location = new System.Drawing.Point(588, 12);
+			this.Boton1.Name = "Boton1";
+			this.Boton1.Size = new System.Drawing.Size(98, 27);
+			this.Boton1.TabIndex = 1;
+			this.Boton1.Text = "Mostrar imagen";
+			this.Boton1.UseVisualStyleBackColor = true;
+			this.Boton1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// buttonAnalizar
+			// 
+			this.buttonAnalizar.AccessibleName = "ShowImage";
+			this.buttonAnalizar.Location = new System.Drawing.Point(588, 55);
+			this.buttonAnalizar.Name = "buttonAnalizar";
+			this.buttonAnalizar.Size = new System.Drawing.Size(98, 27);
+			this.buttonAnalizar.TabIndex = 2;
+			this.buttonAnalizar.Text = "Analizar imagen";
+			this.buttonAnalizar.UseVisualStyleBackColor = true;
+			this.buttonAnalizar.Click += new System.EventHandler(this.ButtonAnalizarClick);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(588, 109);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(110, 108);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Centro";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(718, 345);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.buttonAnalizar);
+			this.Controls.Add(this.Boton1);
+			this.Controls.Add(this.pictureBoximage);
+			this.Name = "MainForm";
+			this.Text = "Circulos";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoximage)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button buttonAnalizar;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button Boton1;
+		private System.Windows.Forms.PictureBox pictureBoximage;
+	}
+}
---
+++ Circulos/MainForm.cs
@@ -0,0 +1,138 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DIANA
+ * Date: 16/03/2021
+ * Time: 05:20 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Circulos
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		
+		}
+		
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+			pictureBoximage.Image = Image.FromFile(openFileDialog1.FileName);
+		}
+		
+		class objetocirculo{
+		//public:
+			int radio;
+			int x;
+			int y;
+			int diametro;
+		};
+		
+		void ButtonAnalizarClick(object sender, EventArgs e)
+		{
+			int x=0,y=0;
+			
+			
+			Bitmap bmpcentro = new Bitmap(openFileDialog1.FileName);
+			Color colory;
+			pictureBoximage.Image = bmpcentro;
+			colory=bmpcentro.GetPixel(x,y );
+				for( x=0; x < bmpcentro.Height; x++)
+					for( y=0; y < bmpcentro.Width; y++){//recorrer todos los pixeles
+					
+					if(colory.R!=255 && colory.G!=255&&colory.B!=255){//verificar que no sea blanco
+		                 BuscaCentro(x,y,bmpcentro);
+					}
+		
+		}
+		}
+		
+		
+		void BuscaCentro(int x,int y,Bitmap image){
+		int yi,xi,yf,xf;
+		int yc=0,xc=0;
+		Color pixcol;
+		pixcol=image.GetPixel(x,y);
+				       yi=y;//Y inicial
+				       yf=y;//y final
+				       while(pixcol.R!=255 && pixcol.G!=255&& pixcol.B!=255){
+				       	  pixcol=image.GetPixel(x,yf);
+				      	  yf++;
+				      	  
+						}
+						
+				       yc=(yi+yf)/2;//y centro
+						
+						xi=x;
+						xf=x;
+						while(pixcol.R!=255 && pixcol.G!=255&& pixcol.B!=255){
+							pixcol=image.GetPixel(x,yc);
+							xi--;
+							
+						}
+						//x inicial
+						while(pixcol.R!=255 && pixcol.G!=255&& pixcol.B!=255){
+							pixcol=image.GetPixel(x,yc);
+							xf++;
+						}
+						//x final
+						xc=(xi+xf)/2;//x central
+						
+						
+					//}
+					
+				
+				 
+				
+				label1.Text = ("Centro: "+xc.ToString()+ ","+yc.ToString());
+				
+				//int i =  xi;
+				//int j = yi;
+				
+			
+		}//point
+		
+		
+		
+		void dibujacirculo(int f,int c){
+			
+		}
+	
+		
+
+	}
+}
+/*
+
+			Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+			Color colorActual;
+			pictureBoximage.Image = bmp;
+			
+			for(int i=0; i < bmp.Height; i++)
+				for(int j=0; j < bmp.Width; j++){
+				
+				   colorActual = bmp.GetPixel(j,i);
+			       if(colorActual.R!=255)
+			          if(colorActual.G!=255)
+					     if(colorActual.B!=255)
+						    bmp.SetPixel(j,i,Color.Blue);
+			}
+		*/	
